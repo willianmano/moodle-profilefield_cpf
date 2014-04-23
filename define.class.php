e4f00b011f8d7caa9669eeb034d3ab84cfe24094
Willian Mano <willianmanoaraujo@gmail.com>
2014-04-22 23:05:57 -0300

    Fix code conventions

--- define.class.php
+++ define.class.php
@@ -1,8 +1,7 @@
 <?php
 
 class profile_define_cpf extends profile_define_base {
-
-    function define_form_specific($form) {
+    public function define_form_specific($form) {
         // Default data.
         $form->addElement('text', 'defaultdata', get_string('profiledefaultdata', 'admin'));
         $form->setType('defaultdata', PARAM_TEXT);
--- field.class.php
+++ field.class.php
@@ -1,18 +1,21 @@
 <?php
 
 class profile_field_cpf extends profile_field_base {
-
-    function edit_field_add($mform) {
-        $mform->addElement('text', $this->inputname, format_string($this->field->name), 'maxlength="14" size="14" id="profilefield_cpf"');
+    public function edit_field_add($mform) {
+        $mform->addElement(
+            'text',
+            $this->inputname,
+            format_string($this->field->name),
+            'maxlength="14" size="14" id="profilefield_cpf"'
+        );
         $mform->setType($this->inputname, PARAM_TEXT);
     }
-    function edit_validate_field($usernew) {
+    public function edit_validate_field($usernew) {
         $return = array();
         if (isset($usernew->{$this->inputname})) {
-            if ( !$this->exists($usernew->{$this->inputname}, $usernew->id)) {
+            if (!$this->exists($usernew->{$this->inputname}, $usernew->id)) {
                 $return[$this->inputname] = get_string('cpfexists', 'profilefield_cpf');
-            }
-            else if ( !$this->validateCPF($usernew->{$this->inputname})) {
+            } else if (!$this->validatecpf($usernew->{$this->inputname})) {
                 $return[$this->inputname] = get_string('invalidcpf', 'profilefield_cpf');
             }
         }
@@ -21,7 +24,7 @@ class profile_field_cpf extends profile_field_base {
     private function exists($cpf = null, $userid = 0) {
         global $DB;
         // Verifica se um número foi informado.
-        if(is_null($cpf)) {
+        if (is_null($cpf)) {
             return false;
         }
 
@@ -30,17 +33,17 @@ class profile_field_cpf extends profile_field_base {
                 WHERE uif.datatype = 'cpf' AND uid.data = :cpf AND uid.userid <> :userid";
         $params['cpf'] = $cpf;
         $params['userid'] = $userid;
-        $db_cpf = current($DB->get_records_sql($sql, $params));
-        
-        if(!empty($db_cpf)) {
+        $dbcpf = current($DB->get_records_sql($sql, $params));
+
+        if (!empty($dbcpf)) {
             return false;
         } else {
             return true;
         }
     }
-    private function validateCPF($cpf = null) {
+    private function validatecpf($cpf = null) {
         // Verifica se um numero foi informado.
-        if(is_null($cpf)) {
+        if (is_null($cpf)) {
             return false;
         }
         // Elimina possivel mascara.
@@ -50,15 +53,15 @@ class profile_field_cpf extends profile_field_base {
         // Verifica se o numero de digitos informados eh igual a 11.
         if (strlen($cpf) != 11) {
             return false;
-        }
-        // Verifica se nenhuma das sequencias invalidas abaixo.
-        // Foi digitada. Caso afirmativo, retorna falso.
-        else if ($cpf == '00000000000' || $cpf == '11111111111' || $cpf == '22222222222' || $cpf == '33333333333' || $cpf == '44444444444' ||         $cpf == '55555555555' || $cpf == '66666666666' || $cpf == '77777777777' || $cpf == '88888888888' || $cpf == '99999999999') {
+        } else if ($cpf == '00000000000' || $cpf == '11111111111' || $cpf == '22222222222' ||
+            $cpf == '33333333333' || $cpf == '44444444444' || $cpf == '55555555555' ||
+            $cpf == '66666666666' || $cpf == '77777777777' || $cpf == '88888888888' ||
+            $cpf == '99999999999') {
             return false;
-         // Calcula os digitos verificadores para verificar se o CPF eh valido.
-         } else {   
+        } else {
+            // Calcula os digitos verificadores para verificar se o CPF eh valido.
             for ($t = 9; $t < 11; $t++) {
-                 
+
                 for ($d = 0, $c = 0; $c < $t; $c++) {
                     $d += $cpf{$c} * (($t + 1) - $c);
                 }
@@ -67,7 +70,7 @@ class profile_field_cpf extends profile_field_base {
                     return false;
                 }
             }
-     
+
             return true;
         }
     }
